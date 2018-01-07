50b4a0ddd53c3ed2612c9876810f522538b74370
Николай <adslbarxatov@mail.ru>
2018-01-07 18:32:31 +1100

    Assembly v 1.1 (7.01.18)

---
+++ MainForm.cs
@@ -0,0 +1,203 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace ScreenShooter
+	{
+	/// <summary>
+	/// Начальная форма программы
+	/// </summary>
+	public partial class MainForm:Form
+		{
+		// Параметры
+		private Point start, end;
+		private Graphics g;
+		private Bitmap b;
+
+		/// <summary>
+		/// Главная форма программы
+		/// </summary>
+		public MainForm ()
+			{
+			InitializeComponent ();
+
+			// Настройка
+			this.Text = ProgramDescription.AssemblyTitle;
+
+			this.Left = this.Top = 0;
+			this.Width = Screen.PrimaryScreen.Bounds.Width;
+			this.Height = Screen.PrimaryScreen.Bounds.Height;
+
+			SFDialog.Title = "Укажите путь для сохранения изображения";
+			SFDialog.Filter = "Изображение PNG|*.png|Фотография JPEG|*.jpeg|Рисунок BMP|*.bmp|Рисунок GIF|*.gif";
+			}
+
+		// Нажатие мыши
+		private void MainForm_MouseDown (object sender, MouseEventArgs e)
+			{
+			if (e.Button == System.Windows.Forms.MouseButtons.Right)
+				{
+				SaveImage ();
+				return;
+				}
+
+			if (e.Button != System.Windows.Forms.MouseButtons.Left)
+				return;
+
+			if (!MainSelection.Visible)
+				MainSelection.Visible = true;
+
+			MainSelection.Location = start = e.Location;
+			}
+
+		// Движение мыши
+		private void MainForm_MouseMove (object sender, MouseEventArgs e)
+			{
+			if (e.Button != System.Windows.Forms.MouseButtons.Left)
+				return;
+
+			if (e.X >= start.X)
+				{
+				MainSelection.Left = start.X;
+				MainSelection.Width = e.X - start.X + 1;
+				}
+			else
+				{
+				MainSelection.Left = e.X;
+				MainSelection.Width = start.X - e.X + 1;
+				}
+
+			if (e.Y >= start.Y)
+				{
+				MainSelection.Top = start.Y;
+				MainSelection.Height = e.Y - start.Y + 1;
+				}
+			else
+				{
+				MainSelection.Top = e.Y;
+				MainSelection.Height = start.Y - e.Y + 1;
+				}
+
+			MainSelection.Text = "(" + MainSelection.Left.ToString () + "; " + MainSelection.Top.ToString () + ") (" +
+				MainSelection.Width.ToString () + " x " + MainSelection.Height.ToString () + ")";
+			}
+
+		// Завершение выделения
+		private void MainForm_MouseUp (object sender, MouseEventArgs e)
+			{
+			end = e.Location;
+			}
+
+		// Передача управления на форму
+		private void MainSelection_MouseDown (object sender, MouseEventArgs e)
+			{
+			if (e.Button == System.Windows.Forms.MouseButtons.Right)
+				{
+				SaveImage ();
+				return;
+				}
+
+			if (MainSelection.Visible)
+				MainSelection.Visible = false;
+			}
+
+		// Обработка клавиатуры
+		private void MainForm_KeyDown (object sender, KeyEventArgs e)
+			{
+			switch (e.KeyCode)
+				{
+				// Справка
+				case Keys.F1:
+					MessageBox.Show (ProgramDescription.AssemblyTitle + "\n" + ProgramDescription.AssemblyDescription +
+						"\n\nВерсия " + ProgramDescription.AssemblyVersion +
+						" от " + ProgramDescription.AssemblyLastUpdate + "\n" + ProgramDescription.AssemblyCopyright + "\n\n" +
+
+						"• Левая кнопка мыши (нажать и тянуть) – выбор области снимка;\n" +
+						"• Левый щелчок по выбранной области или клавиша Пробел – сброс выделения;\n" +
+						"• Правая кнопка мыши или клавиша Enter – сохранение изображения;\n" +
+						"• F1 – вызов этого справочного сообщения;\n" +
+						"• Q, X, Esc, Alt + F4 – выход из программы",
+						ProgramDescription.AssemblyTitle, MessageBoxButtons.OK, MessageBoxIcon.Information);
+					break;
+
+				// Сохранение
+				case Keys.Return:
+					SaveImage ();
+					break;
+
+				// Выход
+				case Keys.Escape:
+				case Keys.X:
+				case Keys.Q:
+					this.Close ();
+					break;
+
+				// Сброс выделения
+				case Keys.Space:
+					MainSelection.Visible = false;
+					break;
+				}
+			}
+
+		// Сохранение изображения
+		private void SaveImage ()
+			{
+			// Фиксация размера
+			if (!MainSelection.Visible)
+				{
+				start.X = 0;
+				start.Y = 0;
+				end.X = Screen.PrimaryScreen.Bounds.Width - 1;
+				end.Y = Screen.PrimaryScreen.Bounds.Height - 1;
+				}
+
+			// Получение дескриптора и снимка экрана
+			if (b != null)
+				{
+				b.Dispose ();
+				}
+			b = new Bitmap (end.X - start.X + 1, end.Y - start.Y + 1);
+
+			g = Graphics.FromImage (b);
+			g.CopyFromScreen (start.X, start.Y, 0, 0, new Size (end.X - start.X + 1, end.Y - start.Y + 1));
+			g.Dispose ();
+
+			// Запрос имени файла (делается после снимка, чтобы не перекрывать экран)
+			SFDialog.ShowDialog ();
+			}
+
+		private void SFDialog_FileOk (object sender, System.ComponentModel.CancelEventArgs e)
+			{
+			// Обработка параметров
+			try
+				{
+				switch (SFDialog.FilterIndex)
+					{
+					case 1:
+						b.Save (SFDialog.FileName, ImageFormat.Png);
+						break;
+
+					case 2:
+						b.Save (SFDialog.FileName, ImageFormat.Jpeg);
+						break;
+
+					case 3:
+						b.Save (SFDialog.FileName, ImageFormat.Bmp);
+						break;
+
+					case 4:
+						b.Save (SFDialog.FileName, ImageFormat.Gif);
+						break;
+					}
+				}
+			catch
+				{
+				MessageBox.Show ("Не удалось сохранить изображение. Возможно, целевая директория недоступна для записи",
+					ProgramDescription.AssemblyTitle, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+				}
+
+			// Завершение
+			b.Dispose ();
+			}
+		}
+	}
